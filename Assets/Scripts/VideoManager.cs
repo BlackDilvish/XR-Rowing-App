f6808f9ea5e399b8fb6b48ff366d25b6b1133587
BlackDilvish <gabriel.naleznik@gmail.com>
2021-10-17 23:25:38 +0200

    Create clips library in runtime

--- Assets/Scripts/VideoManager.cs
+++ Assets/Scripts/VideoManager.cs
@@ -16,8 +16,8 @@ public class VideoManager : MonoBehaviour
 {
     public Text downloadingInfo;
 
-    public static UsedVideoType videoType;
-    private static string videoName;
+    public static UsedVideoType videoType = UsedVideoType.Local;
+    private static string currentVideoName = "Clip1";
     private static string basePath = "C:/C#/Unity/Images/Road/";
     private static string videoDataPath = "Assets/Resources/Data/video_data.txt";
     private static Dictionary<string, string> videoData = new Dictionary<string, string>();
@@ -26,11 +26,12 @@ public class VideoManager : MonoBehaviour
     {
         DontDestroyOnLoad(transform.gameObject);
         InitVideoData();
+        InstantiateVideoOptions();
     }
 
     public static string GetCurrentVideoPath()
     {
-        return basePath + videoName;
+        return basePath + currentVideoName + ".mp4";
     }
 
     public void StartDownloadingVideo()
@@ -39,14 +40,19 @@ public class VideoManager : MonoBehaviour
         Debug.Log(selectedVideo);
         string url = videoData[selectedVideo];
         string path = basePath + selectedVideo + ".mp4";
+        downloadingInfo.gameObject.SetActive(true);
         StartCoroutine(DownloadVideo(url, path));
     }
 
     public void SetCurrentVideo()
     {
-        var selectedVideo = EventSystem.current.currentSelectedGameObject.transform.parent.gameObject.name;
-        videoType = selectedVideo == "Clip1" ? UsedVideoType.Local : UsedVideoType.Downloaded;
-        videoName = selectedVideo + ".mp4";
+        var selectedVideo = EventSystem.current.currentSelectedGameObject.transform.parent;
+        var selectedVideoText = selectedVideo.GetChild(0).GetComponent<Text>();
+        GameObject.Find(currentVideoName).transform.GetChild(0).GetComponent<Text>().color = Color.black;
+        selectedVideoText.color = Color.red;
+        Debug.Log(selectedVideoText.text);
+        videoType = selectedVideoText.text == "Clip1" ? UsedVideoType.Local : UsedVideoType.Downloaded;
+        currentVideoName = selectedVideoText.text;
     }
 
     IEnumerator DownloadVideo(string url, string path)
@@ -63,7 +69,7 @@ public class VideoManager : MonoBehaviour
         if (uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.ProtocolError)
             Debug.Log(uwr.error);
         else
-            Debug.Log("Download saved to: " + uwr.error);
+            Debug.Log("Download saved to: " + path);
 
         downloadingInfo.gameObject.SetActive(false);
     }
@@ -75,7 +81,35 @@ public class VideoManager : MonoBehaviour
             string[] splitted = line.Split();
             string videoName = splitted[0];
             string videoUrl = splitted[1];
-            videoData.Add(videoName, videoUrl);
+            if (!videoData.ContainsKey(videoName))
+            {
+                videoData.Add(videoName, videoUrl);
+            }
         }
+    }
+
+    private void InstantiateVideoOptions()
+    {
+        Vector2 position = new Vector2(0, 50);
+        var canvas = GameObject.Find("ScrollableArea");
+        var videoOption = Resources.Load("RuntimePrefabs/ClipOption", typeof(GameObject)) as GameObject;
+
+        foreach (string line in File.ReadAllLines(videoDataPath))
+        {
+            string[] splitted = line.Split();
+            string videoName = splitted[0];
+            
+            GameObject newOption = Instantiate(videoOption, canvas.transform);
+            newOption.name = videoName;
+            newOption.transform.GetChild(0).GetComponent<Text>().text = videoName;
+            newOption.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(StartDownloadingVideo);
+            newOption.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(SetCurrentVideo);
+            newOption.transform.SetParent(canvas.transform);
+            newOption.GetComponent<RectTransform>().anchoredPosition = position;
+
+            position.y -= 30;
+        }
+
+        GameObject.Find("LibraryCanvas").SetActive(false);
     }
 }
