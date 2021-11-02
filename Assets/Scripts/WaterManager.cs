c87f54aaf363b8ce5810d74ba3e839b92f9a596f
BlackDilvish <gabriel.naleznik@gmail.com>
2021-11-02 20:14:03 +0100

    Fix waves and obstacles in frames mode

--- Assets/Scripts/WaterManager.cs
+++ Assets/Scripts/WaterManager.cs
@@ -8,6 +8,11 @@ public class WaterManager : MonoBehaviour
 {
     private MeshFilter m_meshFilter;
 
+    public float amplitude = 1f;
+    public float length = 2f;
+    public float speed = 1f;
+    public float offset = 0f;
+
     void Start()
     {
         m_meshFilter = GetComponent<MeshFilter>();
@@ -15,13 +20,20 @@ public class WaterManager : MonoBehaviour
 
     void Update()
     {
+        offset += Time.deltaTime + speed;
+
         Vector3[] vertices = m_meshFilter.mesh.vertices;
-        for (int i = 0; i<vertices.Length; i++)
+        for (int i = 0; i < vertices.Length; i++)
         {
-            vertices[i].y = WaveManager.instance.GetWaveHeight(transform.position.x + vertices[i].x);
+            vertices[i].y = GetWaveHeight(vertices[i].x);
         }
 
         m_meshFilter.mesh.vertices = vertices;
         m_meshFilter.mesh.RecalculateNormals();
+    }
+
+    public float GetWaveHeight(float x)
+    {
+        return amplitude * Mathf.Sin(x / length + offset);
     }
 }
